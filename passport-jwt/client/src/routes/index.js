2e15f802e45187c97ef032da5b6171fdba48be1e
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-06-13 10:02:25 +0600

    client environment setup with routes

---
+++ passport-jwt/client/src/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../components/Home';
+import Register from '../components/Register';
+import Login from '../components/Login';
+import Error from '../components/Error';
+
+const Index = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Index;
--- passport-jwt/server/config/passport.js
+++ passport-jwt/server/config/passport.js
@@ -11,32 +11,6 @@ opts.secretOrKey = process.env.SECRET_KEY;
 
 passport.use(
   new JwtStrategy(opts, async function (jwt_payload, done) {
-    console.log(jwt_payload);
-    // User.findOne({ id: jwt_payload.id }, function (err, user) {
-    //   if (err) {
-    //     return done(err, false);
-    //   }
-    //   if (user) {
-    //     return done(null, user);
-    //   } else {
-    //     return done(null, false);
-    //     // or you could create a new account
-    //   }
-    // });
-
-    // User.findOne({ id: jwt_payload.id })
-    //   .then((user) => {
-    //     if (user) {
-    //       return done(null, user);
-    //     } else {
-    //       return done(null, false);
-    //       // or you could create a new account
-    //     }
-    //   })
-    //   .catch((error) => {
-    //     console.log(error.message);
-    //   });
-
     try {
       const user = await User.findOne({ id: jwt_payload._id });
 
