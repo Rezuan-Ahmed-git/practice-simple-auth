8577d11357913cd43752a992b234e9533e73ea2b
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-06-13 10:16:47 +0600

    feat: register feature implement

--- passport-jwt/client/src/components/Register.jsx
+++ passport-jwt/client/src/components/Register.jsx
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleRegister = () => {
+    axios
+      .post('http://localhost:4000/register', { username, password })
+      .then(() => {
+        console.log('user is registered');
+        navigate('/login');
+      })
+      .catch((error) => {
+        console.log(error);
+        navigate('/register');
+      });
+  };
+
   return (
     <div>
       <h2>Register Page</h2>
@@ -24,6 +41,9 @@ const Register = () => {
         }}
         required
       />
+      <button type="submit" onClick={handleRegister}>
+        Register
+      </button>
     </div>
   );
 };
