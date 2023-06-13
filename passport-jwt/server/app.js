56f0b851bc1408248a24774dc6174037b1418dd8
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-06-13 06:27:56 +0600

    basic route setup

--- passport-jwt/server/app.js
+++ passport-jwt/server/app.js
@@ -13,6 +13,16 @@ app.get('/', (req, res) => {
   res.send('<h1>Welcome to the server</h1>');
 });
 
+//register route
+app.post('/register', (req, res) => {
+  res.send('<h1>Welcome to the register</h1>');
+});
+
+//login route
+app.post('/login', (req, res) => {
+  res.send('<h1>Welcome to the login</h1>');
+});
+
 //resource not found
 app.use((req, res, next) => {
   res.status(404).json({
